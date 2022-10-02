50c13acff7f464707ef990423cda2b53026f8ad8
ibrartt <111246165+ibrar19911@users.noreply.github.com>
2022-10-02 10:20:28 +0500

    First commit after setting up basic structure, phpcs, and unit testing.

---
+++ wordpress-slideshow-i.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: WordPress Slideshow
+ * Plugin URI: https://www.example.com
+ * Description: This is an assignment plugin with which we can assign multiple authors to one post.
+ * Version: 1.0.0
+ * Author: Muhammad Ibrar
+ * Author URI: https://profiles.wordpress.org/ibrar1991/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wordpress-slideshow
+ * Domain Path: /languages
+ *
+ * @package WordPress Slideshow
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+// Important Constants
+define( 'WPSI_CSS_URL', plugins_url( 'wordpress-slideshow-i' ) . '/css/' );
+define( 'WPSI_JS_URL', plugins_url( 'wordpress-slideshow-i' ) . '/js/' );
+
+// Inlude files.
+include_once 'admin/class-wpsi-add-menu-pages.php';
