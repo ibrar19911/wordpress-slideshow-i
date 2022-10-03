1b41b9a7c59c1b4927cd0eababde35198fb91841
ibrartt <111246165+ibrar19911@users.noreply.github.com>
2022-10-03 10:26:17 +0500

    Slideshow slider initiated, and styled a little to make images fit in layout.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+
+	jQuery('.wpsi-slideshow').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: true,
+		adaptiveHeight: true
+	});
+
+});
